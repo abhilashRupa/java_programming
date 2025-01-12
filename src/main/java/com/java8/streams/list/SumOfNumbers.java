6fd4ea6bd79145597291daab34a45f62d83fd82f
Abhilash Rupa <abhilashrupa@gmail.com>
2025-01-12 23:24:13 +0530

    sum of given numbers

--- src/main/java/com/java8/streams/list/SumOfNumbers.java
+++ src/main/java/com/java8/streams/list/SumOfNumbers.java
@@ -1,2 +1,22 @@
-package com.java8.streams.list;public class SumOfNumbers {
+package com.java8.streams.list;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+* Write a program to find the sum of all elements in a list using Java Stream API
+* */
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        int sumOfNumber = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println(sumOfNumber);
+
+    }
+
 }
