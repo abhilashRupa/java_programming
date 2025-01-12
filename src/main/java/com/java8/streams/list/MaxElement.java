b4916c772ce61cd47bea0d39721ceaf874406bdd
Abhilash Rupa <abhilashrupa@gmail.com>
2025-01-12 17:37:26 +0530

    add problem statement

--- src/main/java/com/java8/streams/list/MaxElement.java
+++ src/main/java/com/java8/streams/list/MaxElement.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalInt;
 
+//given a list of integers, write a program to find and print the maximum element using java Stream API
+
 public class MaxElement {
     public static void main(String[] args) {
         List<Integer> numberList = Arrays.asList(1, 3, 5, 6, 75, 34, 667, 33);
---
+++ src/main/java/com/java8/streams/list/SumOfNumbers.java
@@ -0,0 +1,2 @@
+package com.java8.streams.list;public class SumOfNumbers {
+}
