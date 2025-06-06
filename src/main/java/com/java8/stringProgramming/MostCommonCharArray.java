c335145832751543e74b89b40c1d193e880b90ab
Abhilash Rupa <abhilashrupa@gmail.com>
2025-06-06 23:00:28 +0530

    most common char in an array

--- src/main/java/com/java8/stringProgramming/MostCommonCharArray.java
+++ src/main/java/com/java8/stringProgramming/MostCommonCharArray.java
@@ -9,62 +9,67 @@ import java.util.stream.Collectors;
 
 public class MostCommonCharArray {
 
-  public static void main(String[] args) {
-    String[] input = {"geeksforgeeks", "geeks", "geek", "geezer"};
+    public static void main(String[] args) {
+        String[] input = {"geeksforgeeks", "geeks", "geek", "geezer" };
 
-    MostCommonCharArray mostCommonCharArray = new MostCommonCharArray();
-    System.out.println(mostCommonCharArray.approach1(input));
-    System.out.println(mostCommonCharArray.approach2(input));
-    System.out.println(mostCommonCharArray.approach3(input));
+        MostCommonCharArray mostCommonCharArray = new MostCommonCharArray();
+        System.out.println(mostCommonCharArray.approach1(input));
+        System.out.println(mostCommonCharArray.approach2(input));
+        System.out.println(mostCommonCharArray.approach3(input));
 
 
-  }
+    }
 
-  private char approach1(String[] input) {
+    private char approach1(String[] input) {
 
-    String concatString = String.join("", input);
+        String concatString = String.join("", input);
 
-    Optional<Map.Entry<String, Long>> mostCommonCharacter =
-        concatString
-            .chars()
-            .mapToObj(ch -> (char) ch)
-            .collect(Collectors.groupingBy(Objects::toString, Collectors.counting()))
-            .entrySet()
-            .stream()
-            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-            .findFirst();
+        Optional<Map.Entry<String, Long>> mostCommonCharacter =
+                concatString
+                        .chars()
+                        .mapToObj(ch -> (char) ch)
+                        .collect(Collectors.groupingBy(Objects::toString, Collectors.counting()))
+                        .entrySet()
+                        .stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .findFirst();
 
-    return mostCommonCharacter.get().getKey().charAt(0);
-  }
+        return mostCommonCharacter.get().getKey().charAt(0);
+    }
 
-  private char approach2(String[] input){
+    private char approach2(String[] input) {
 
-    String concarString = String.join("", input);
+        String concarString = String.join("", input);
 
-    Optional<Map.Entry<Character, Long>> mostCommonEntry = concarString.chars()
-            .mapToObj(ch -> (char)ch)
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-            .entrySet()
-            .stream()
-            .sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()))
-            .findFirst();
+        Optional<Map.Entry<Character, Long>> mostCommonEntry = concarString.chars()
+                .mapToObj(ch -> (char) ch)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()))
+                .findFirst();
 
-    return mostCommonEntry.get().getKey();
+        return mostCommonEntry.get().getKey();
 
-  }
+    }
 
-  private char approach3(String[] input){
+    private char approach3(String[] input) {
 
-    String concarString = String.join("", input);
+        String concarString = String.join("", input);
 
-    Optional<Map.Entry<Character, Long>> mostCommonEntry = concarString.chars()
-            .mapToObj(ch -> (char)ch)
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-            .entrySet()
-            .stream()
-            .max(Comparator.comparingLong(Map.Entry::getValue));
+        Optional<Map.Entry<Character, Long>> mostCommonEntry = concarString.chars()
+                .mapToObj(ch -> (char) ch)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingLong(Map.Entry::getValue));
 
-    return mostCommonEntry.get().getKey();
+        return mostCommonEntry.get().getKey();
 
-  }
+    }
+
+    /*todo -
+     *  try another approach by iterating over array and collecting the count of each char and getting the most common
+     *
+     * */
 }
